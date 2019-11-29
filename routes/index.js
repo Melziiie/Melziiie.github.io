93b7c6a127d7d3c86f8e3f5a218eeb56e4a5274a
Melziiie <rotasha.medford@gmail.com>
2019-11-29 09:48:20 +0800

    Node JS and Express Initial

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '1958' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: '1958' });
+});
+
+/* GET login page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: '1958' });
+});
+
+module.exports = router;
