322b125b4f4530766ea4d1c5f01b347dddbd653c
Melziiie <rotasha.medford@gmail.com>
2019-12-31 12:25:27 +0800

    Last Change

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,23 @@ router.post('/register', ctrl.register);
 /* GET product page. */
 router.get('/:id', ctrl.product);
 
-router.post('/product', ctrl.upload);
+router.post('/product', ctrl.uploadProduct);
 
+router.get('/productManager', ctrl.loadProduct);
+router.post('/productManager', function(req, res, next){
+  console.log(req.body)
+  if (req.body.Type == "delete"){
+    ctrl.removeProduct(req, res);
+  }
+  if (req.body.Type == "modify"){
+    ctrl.modifyProduct(req, res);
+  }
+});
+
+router.get('/productModifier', ctrl.loadProductModifier);
+router.post('/productModifier', ctrl.saveProductModification)
+
+router.get('/checkout', function(req, res, next){
+  res.render('checkout');
+});
 module.exports = router;
